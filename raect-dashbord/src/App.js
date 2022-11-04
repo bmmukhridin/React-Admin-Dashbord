c81d21fd6f35bd64d24f7f3e0bdbb30f5dd9fac3
bmmukhridin <bmmukhridin@icloud.com>
2022-11-03 18:09:28 -0700

    added-routes(react-dom-router)

--- raect-dashbord/src/App.js
+++ raect-dashbord/src/App.js
@@ -1,9 +1,38 @@
+import { Home } from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New"
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+    <Routes>
+     <Route path="/">
+      <Route index element={<Home />}></Route>
+      <Route path="login" element={<Login />}></Route>
+      <Route path="users">
+        <Route index element={<List />}></Route>
+        <Route path=":userID" element={<Single />}></Route>
+        <Route path="new" element={<New />}></Route>
+      </Route>
+      <Route path="products">
+        <Route index element={<List />}></Route>
+        <Route path=":productID" element={<Single />}></Route>
+        <Route path="new" element={<New />}></Route>
+      </Route>
+     </Route>
+
+    </Routes>
+  </BrowserRouter>
     </div>
   );
 }
---
+++ raect-dashbord/src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../home/home.scss"
+
+export const Home = () => {
+  return <div className="home">
+    <h1 className="title">Dashboard</h1>
+  </div>;
+};
